873fa5ba2c1cc8ef7c53543bed6e51489a5d0c37
Rafael Prado <contato@rprado.design>
2022-08-15 15:03:47 -0300

    refactor: add basic files

---
+++ assets/scripts/main.js
@@ -0,0 +1,7 @@
+// artigo
+// https://meajuda.picpay.com/hc/pt-br/articles/360058187372
+// picpay://picpay/helpcenter/article/360058187372
+
+// secao
+// https://meajuda.picpay.com/hc/pt-br/sections/360013672411
+// picpay://picpay/helpcenter/section/360013672411
